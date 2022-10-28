9949ea9185ba5338bea3ab7ff915473672af544c
DandinPower <tomhot246@gmail.com>
2022-10-28 15:29:45 +0800

    adjust layout

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 from dotenv import load_dotenv
-from src.dataLoader import DataLoader
+from preprocess.src.dataLoader import DataLoader
 load_dotenv()
 DATASET_TYPE = os.getenv('DATASET_TYPE')
 DATASET_PATH = os.getenv('DATASET_PATH')
@@ -18,6 +18,11 @@ def main():
     dataLoader = DataLoader()
     dataLoader.SetDataset(DATASET_TYPE, DATASET_PATH, MAX_LEN, SPLIT_RATES, BATCH_SIZE)
     trainDataLoader = dataLoader.GetTrainDataLoader()
+    x = 0
+    for a in trainDataLoader:
+        if x == 0:
+            print(a)
+        x += 1
 
 if __name__ == "__main__":
     main()
