620219e50e5e89bc5237ba268d3bb296b471127f
DandinPower <tomhot246@gmail.com>
2022-11-26 16:56:06 +0800

    add skrm write data

--- models/bert/logger/logger/skrmSimulator.py
+++ models/bert/logger/logger/skrmSimulator.py
@@ -1,3 +1,5 @@
+import csv
+
 class SKRM:
     def __init__(self):
         self.shifts = 0
@@ -27,3 +29,10 @@ class SKRM:
 
     def Show(self):
         print(f'shifts: {self.shifts}, latency: {self.shifts * 5e-10}, energy: {self.shifts * 20}; detects: {self.detects}, latency: {self.detects * 1e-10}, energy: {self.detects * 2}; inserts: {self.inserts}, latency: {self.inserts * 1e-9}, energy: {self.inserts * 200}; removes: {self.removes}, latency: {self.removes * 8e-10}, energy: {self.removes * 20}; total latency: {self.CountLatency()}s, total energy: {self.CountEnergy()}fJ')
+
+    def WriteShow(self, path):
+        with open(path, 'w') as csvfile:
+            fieldnames = ['shifts', 'shifts_latency', 'shifts_energy', 'detects', 'detects_latency', 'detects_energy', 'inserts', 'inserts_latency', 'inserts_energy', 'removes', 'removes_latency', 'removes_energy', 'total_latency', 'total_energy']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerow({'shifts': f'{self.shifts}', 'shifts_latency': f'{self.shifts * 5e-10}', 'shifts_energy': f'{self.shifts * 20}', 'detects': f'{self.detects}', 'detects_latency': f'{self.detects * 1e-10}', 'detects_energy': f'{self.detects * 2}', 'inserts': f'{self.inserts}', 'inserts_latency': f'{self.inserts * 1e-9}', 'inserts_energy': f'{self.inserts * 200}', 'removes': f'{self.removes}', 'removes_latency': f'{self.removes * 8e-10}', 'removes_energy': f'{self.removes * 20}', 'total_latency': f'{self.CountLatency()}', 'total_energy': f'{self.CountEnergy()}'})
--- test.py
+++ test.py
@@ -4,42 +4,14 @@ import os
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+from models.bert.logger.logger.skrmSimulator import SKRM
 
-
-'''
-block = [[4, 4], [4, 4]]
-logger = FullLogger(block)
-logger.ReadLog('train/history/log/lstm_operation.txt')
-logger.ShowNaiveResult(0)
-logger.ShowImproveResult(0)
-'''
-
-
-cola_original_shifts= 2311456 + 7031031660281856
-cola_our_shifts = 2311456 + 3515516040118272
-cola_original_detects = 1093632 + 3515515830140928
-cola_our_detects = 1093632 + 3515515830140928
-cola_original_inserts = 38364
-cola_our_inserts = 0
-cola_original_removes = 55840
-cola_our_removes = 0
-
-stsb_original_shifts= 3861084 + 4687354603503616
-stsb_our_shifts = 3861084 + 2343677395075072
-stsb_original_detects = 1822720 + 2343677301751808
-stsb_our_detects = 1822720 + 2343677301751808
-stsb_original_inserts = 59968
-stsb_our_inserts = 0
-stsb_original_removes = 101724
-stsb_our_removes = 0
-
-lstm_original_shifts= 67844092 + 219167360
-lstm_our_shifts = 67844092 + 118691200
-lstm_original_detects = 0 + 109583680
-lstm_our_detects = 0 + 109583680
-lstm_original_inserts = 717535
-lstm_our_inserts = 0
-lstm_original_removes = 661504
-lstm_our_removes = 0
+skrm = SKRM()
+skrm.shifts = 4578845028437
+skrm.detects = 2102434975744
+skrm.inserts = 75284124902
+skrm.removes = 242572890965
+print(skrm.Show())
+skrm.WriteShow('train/skrm/stsb_approximate_16.csv')
 
 
